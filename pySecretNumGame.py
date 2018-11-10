dd23d544574f5d3c64e68039fbbb76bade4ca939
Pewcodes <33170550+pewcodes@users.noreply.github.com>
2018-11-11 01:02:05 +0800

    Add files via upload

---
+++ pySecretNumGame.py
@@ -0,0 +1,33 @@
+import random
+
+class SecretNumberGame:
+    def __init__(self):
+        self.randomNumber = random.randint(1,100)
+    
+    def generateRandomNumber(self):
+        self.randomNumber = random.randint(1,100)
+
+    def getGuess(self):
+        self.guess = int(input("Your guess on the Secret Number (1-100): "))
+
+    def getResult(self):
+        if self.guess < self.randomNumber:
+            print ("It is higher than..", self.guess)
+        elif self.guess > self.randomNumber:
+            print ("It is lower than..", self.guess)
+        else:
+            print ("B I N G O !")
+    
+    def gameEnded(self):
+        return self.guess != self.randomNumber
+
+print ("Welcome to Guess-the-Secret-Number Game!")
+
+num = SecretNumberGame()
+num.generateRandomNumber() # Generate random number
+num.getGuess() # Get userinput guess
+num.getResult() # Compares guess with the random number, get hints
+
+while num.gameEnded(): # While guess is not correct
+    num.getGuess() # continue to get user input guess
+    num.getResult()
